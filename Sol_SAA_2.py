95a6bd13696893a6f46b621526424f2a70c8b7b8
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-12 15:32:56 +0530

    Create Sol_SAA_2.py

---
+++ Sol_SAA_2.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-100, 100)
+turtle.pendown()
+
+side = 100
+
+turtle.forward(side)
+turtle.left(90)
+
+turtle.forward(side)
+turtle.left(90)
+
+turtle.forward(side)
+turtle.left(90)
+
+turtle.forward(side)
+turtle.left(90)
